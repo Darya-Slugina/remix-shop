085cabb9f05334792a0b6f65c491e1afb040f656
Darya Slugina <d.slugina@gmail.com>
2021-02-12 14:56:24 +0200

    Created carousel skeleton

--- assets/js/index.js
+++ assets/js/index.js
@@ -74,4 +74,45 @@
                 overView.style.display = "none";
         }
     }
+
+    // Carousel
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    let next = document.getElementById("next");
+    next.addEventListener("click", plusSlides);
+
+    let previous = document.getElementById("prev");
+    previous.addEventListener("click", plusSlides);
+
+    // Next/previous controls
+    function plusSlides(e) {
+        if (e.target.id === "next" ? showSlides(slideIndex += 1) : showSlides(slideIndex -= 1));
+    }
+
+    let dots = Array.from(document.getElementsByClassName("dot"));
+    dots.forEach(function (elem) {
+        elem.addEventListener("click", currentSlide);
+    })
+
+    // Thumbnail image controls
+    function currentSlide(e) {
+        showSlides(slideIndex = e.target.id.slice(-1));
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("slideshow");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
 })();
